bef17d98322d7a86fbaf0aa5aec374b24aceb382
Liren <liren90@gmail.com>
2017-01-03 19:06:22 +0800

    added guy without slack in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 User.create(first_name: "Liren", last_name: "Yeo", email: "liren@nextacademy.com")
 User.create(first_name: "Desmond", last_name: "Kang", email: "desmond@nextacademy.com")
 User.create(first_name: "Kevin", last_name: "Sia", email: "kevin@nextacademy.com")
+User.create(first_name: "Guy", last_name: "WihoutSlack", email: "guywithoutslack@nextacademy.com")
 
 Course.create(
     title: "Web Development Bootcamp",
