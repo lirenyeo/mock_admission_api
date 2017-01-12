214844739aea6f5fc0e016745fb469c05c6d7554
Liren <liren90@gmail.com>
2017-01-12 18:54:55 +0800

    seed 1 more user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-User.create(first_name: "Liren", last_name: "Yeo", email: "liren@nextacademy.com")
-User.create(first_name: "Desmond", last_name: "Kang", email: "desmond@nextacademy.com")
-User.create(first_name: "Kevin", last_name: "Sia", email: "kevin@nextacademy.com")
-User.create(first_name: "Guy", last_name: "WihoutSlack", email: "guywithoutslack@nextacademy.com")
+# User.create(first_name: "Liren", last_name: "Yeo", email: "liren@nextacademy.com")
+# User.create(first_name: "Desmond", last_name: "Kang", email: "desmond@nextacademy.com")
+# User.create(first_name: "Kevin", last_name: "Sia", email: "kevin@nextacademy.com")
+# User.create(first_name: "Guy", last_name: "WihoutSlack", email: "guywithoutslack@nextacademy.com")
 
-Course.create(
-    title: "Web Development Bootcamp",
-    course_uuid: "742574f2-0ed5-49c7-91d7-091af00b5acf",
-    slack_token: ENV["slack_token"]
-  )
+# Course.create(
+#     title: "Web Development Bootcamp",
+#     course_uuid: "742574f2-0ed5-49c7-91d7-091af00b5acf",
+#     slack_token: ENV["slack_token"]
+#   )
+
+User.create(first_name: "LirenN", last_name: "YeoO", email: "liren90@gmail.com")
