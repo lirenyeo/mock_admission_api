eb998208066e0f99357adac2ee0fa63506d230ce
Liren <liren90@gmail.com>
2017-01-12 18:51:47 +0800

    simplify slack token controller

--- app/controllers/api/slacks_controller.rb
+++ app/controllers/api/slacks_controller.rb
@@ -1,15 +1,7 @@
 class Api::SlacksController < ApplicationController
 
   def create
-    slack_request = SlackRequest.new(course_uuid: params[:course_uuid])
-    if slack_request.valid?
-      status = :ok
-      response = slack_request.get_slack_token
-    else
-      status = :forbidden
-      response = { error: 'Invalid course_uuid' }
-    end
-
-    render json: response, status: status
+    token = Course.find_by(course_uuid: params[:course_uuid]).slack_token
+    render json: token
   end
 end
